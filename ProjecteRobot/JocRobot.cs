e7ef8cccb89e2c7c6316831d9c3d9c247dc3206f
Emilio Pérez <admin@gmail.com>
2020-11-27 00:10:52 +0100

    Correcio en el taulell i primera aparicio del robot

---
+++ ProjecteRobot/JocRobot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProjecteRobot
+{
+    class JocRobot
+    {
+        Point cap;
+        DireccioRobot direccio;
+
+        public DireccioRobot Direccio { get => direccio; set => direccio = value; }
+        public Point Cap { get => cap; set => cap = value; }
+
+        public JocRobot()
+        {
+            cap = new Point(0, 0);
+            direccio = DireccioRobot.Sud;
+        }
+    }
+    public enum DireccioRobot
+    {
+        Nord,
+        Sud,
+        Est,
+        Oest
+    }
+}
--- ProjecteRobot/MainWindow.xaml.cs
+++ ProjecteRobot/MainWindow.xaml.cs
@@ -7,11 +7,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ProjecteRobot
 {
@@ -20,9 +22,47 @@ namespace ProjecteRobot
     /// </summary>
     public partial class MainWindow : Window
     {
+        public const int ROBOT_HEAD_WIDTH = 35;
+        public const int ROBOT_HEAD_HEIGHT = 35;
+
+        public const int TRESOR_HEAD_WIDTH = 100;
+        public const int TRESOR_HEAD_HEIGHT = 100;
+
+        SolidColorBrush bruhshRobot = new SolidColorBrush(Colors.White);
+        SolidColorBrush brushTresor = new SolidColorBrush(Colors.Yellow);
+
+        JocRobot joc;
+        JocRobot tresor;
+        DispatcherTimer timer;
         public MainWindow()
         {
             InitializeComponent();
+
+            joc = new JocRobot();
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromSeconds(1);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //Pintar.
+            canvas.Children.Clear();
+            Ellipse ellipseRobot = new Ellipse()
+            {
+                Fill = bruhshRobot,
+                Width = ROBOT_HEAD_WIDTH,
+                Height = ROBOT_HEAD_HEIGHT
+            };
+
+            Canvas.SetTop(ellipseRobot, joc.Cap.Y * ROBOT_HEAD_HEIGHT);
+            Canvas.SetLeft(ellipseRobot, joc.Cap.X * ROBOT_HEAD_WIDTH);
+            canvas.Children.Add(ellipseRobot);
+        }
+
+        private void btnIniciaJoc_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
         }
     }
 }
